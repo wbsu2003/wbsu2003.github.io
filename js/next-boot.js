187e16cb587a8aad357019b75a1ccf23970045c0
wbsu2003 <wbsu2003@gmail.com>
2026-05-11 12:58:44 +0800

    Site updated: 2026-05-11 12:58:40

--- js/next-boot.js
+++ js/next-boot.js
@@ -6,6 +6,7 @@ NexT.boot.registerEvents = function() {
 
   NexT.utils.registerScrollPercent();
   NexT.utils.registerCanIUseTag();
+  NexT.utils.updateFooterPosition();
 
   // Mobile top menu bar.
   document.querySelector('.site-nav-toggle .toggle').addEventListener('click', event => {
@@ -16,43 +17,22 @@ NexT.boot.registerEvents = function() {
     document.body.classList.toggle('site-nav-on');
   });
 
-  const duration = 200;
   document.querySelectorAll('.sidebar-nav li').forEach((element, index) => {
     element.addEventListener('click', () => {
-      if (element.matches('.sidebar-toc-active .sidebar-nav-toc, .sidebar-overview-active .sidebar-nav-overview')) return;
-      const sidebar = document.querySelector('.sidebar-inner');
-      const panel = document.querySelector('.sidebar-panel-container');
-      const activeClassName = ['sidebar-toc-active', 'sidebar-overview-active'];
-
-      window.anime({
-        duration,
-        targets   : panel,
-        easing    : 'linear',
-        opacity   : 0,
-        translateY: [0, -20],
-        complete  : () => {
-          // Prevent adding TOC to Overview if Overview was selected when close & open sidebar.
-          sidebar.classList.replace(activeClassName[1 - index], activeClassName[index]);
-          window.anime({
-            duration,
-            targets   : panel,
-            easing    : 'linear',
-            opacity   : [0, 1],
-            translateY: [-20, 0]
-          });
-        }
-      });
+      NexT.utils.activateSidebarPanel(index);
     });
   });
-
-  window.addEventListener('resize', NexT.utils.initSidebarDimension);
 
   window.addEventListener('hashchange', () => {
     const tHash = location.hash;
     if (tHash !== '' && !tHash.match(/%\S{2}/)) {
       const target = document.querySelector(`.tabs ul.nav-tabs li a[href="${tHash}"]`);
-      target && target.click();
+      target?.click();
     }
+  });
+
+  window.addEventListener('tabs:click', e => {
+    NexT.utils.registerCodeblock(e.target);
   });
 };
 
@@ -63,21 +43,31 @@ NexT.boot.refresh = function() {
    * Need to add config option in Front-End at 'scripts/helpers/next-config.js' file.
    */
   CONFIG.prism && window.Prism.highlightAll();
-  CONFIG.fancybox && NexT.utils.wrapImageWithFancyBox();
   CONFIG.mediumzoom && window.mediumZoom('.post-body :not(a) > img, .post-body > img', {
     background: 'var(--content-bg-color)'
   });
   CONFIG.lazyload && window.lozad('.post-body img').observe();
-  CONFIG.pangu && window.pangu.spacingPage();
+  if (CONFIG.pangu) {
+    // Polyfill for requestIdleCallback if not supported
+    if (!window.requestIdleCallback) {
+      window.requestIdleCallback = function(cb) {
+        cb({
+          didTimeout   : false,
+          timeRemaining: () => 100
+        });
+      };
+    }
+    [...document.getElementsByTagName('main')].forEach(e => window.pangu.spacingNode(e));
+  }
 
   CONFIG.exturl && NexT.utils.registerExtURL();
-  NexT.utils.registerCopyCode();
+  NexT.utils.wrapTableWithBox();
+  NexT.utils.registerCodeblock();
   NexT.utils.registerTabsTag();
   NexT.utils.registerActiveMenuItem();
   NexT.utils.registerLangSelect();
   NexT.utils.registerSidebarTOC();
   NexT.utils.registerPostReward();
-  NexT.utils.wrapTableWithBox();
   NexT.utils.registerVideoIframe();
 };
 
@@ -86,8 +76,8 @@ NexT.boot.motion = function() {
   if (CONFIG.motion.enable) {
     NexT.motion.integrator
       .add(NexT.motion.middleWares.header)
-      .add(NexT.motion.middleWares.postList)
       .add(NexT.motion.middleWares.sidebar)
+      .add(NexT.motion.middleWares.postList)
       .add(NexT.motion.middleWares.footer)
       .bootstrap();
   }
